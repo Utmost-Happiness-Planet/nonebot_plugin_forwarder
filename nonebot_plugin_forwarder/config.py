4f41d565e955d1f047b7a63ccdc55469aceebc51
Kl1nge5 <2351859943@qq.com>
2022-04-02 18:50:56 +0800

    fix bugs

--- nonebot_plugin_forwarder/config.py
+++ nonebot_plugin_forwarder/config.py
@@ -2,24 +2,24 @@ from nonebot import get_driver, logger
 
 config = get_driver().config
 
-if not config.hasattr("forwarder_source_group"):
+if not hasattr(config, "forwarder_source_group"):
     forwarder_source_group = [""]
     logger.info("未检测到forwarder_source_group配置")
 else:
     forwarder_source_group = config.forwarder_source_group
 
-if not config.hasattr("forwarder_dest_group"):
+if not hasattr(config, "forwarder_dest_group"):
     forwarder_dest_group = [""]
     logger.info("未检测到forwarder_dest_group配置")
 else:
     forwarder_dest_group = config.forwarder_dest_group
 
-if not config.hasattr("forwarder_prefix"):
+if not hasattr(config, "forwarder_prefix"):
     forwarder_prefix = [""]
     logger.info("未检测到forwarder_prefix配置，默认为['']")
 else:
     forwarder_prefix = config.forwarder_prefix
-if not config.hasattr("forwarder_explict"):
+if not hasattr(config, "forwarder_explict"):
     forwarder_explict = [""]
     logger.info("未检测到forwarder_explict配置，默认为['']")
 else:
