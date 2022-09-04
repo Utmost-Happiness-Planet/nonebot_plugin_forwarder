0108d6ef4c0e48a763a1c01b187ea08be77b8497
Kl1nge5 <2351859943@qq.com>
2022-09-04 15:46:42 +0800

    feat: 实现双向转发 issue#5
    当目标群包含来源群时，不会把消息复读一遍

--- nonebot_plugin_forwarder/__init__.py
+++ nonebot_plugin_forwarder/__init__.py
@@ -15,7 +15,7 @@ msg_matcher = on_message(rule, priority=10, block=False)
 
 async def send_meg(bot: Bot, group_id: str, msg: str):
     logger.debug(f"消息转发至: {group_id}")
-    await bot.send_group_msg(group_id=group_id, message=msg, auto_escape=False)
+    await bot.send_group_msg(group_id=int(group_id), message=msg, auto_escape=False)
 
 
 @msg_matcher.handle()
@@ -24,6 +24,6 @@ async def _(bot: Bot, event: GroupMessageEvent):
         flag = forwarder_explict[0] == "" or str(event.user_id) in forwarder_explict
         if flag and forwarder_dest_group[0] != "":
             msg = str(event.message)
-            logger.debug(f"欲转发消息: {msg}")
-            tasks = [send_meg(bot, gid, msg) for gid in forwarder_dest_group]
+            logger.debug(f"欲转发消息: {msg} | 来源: {event.group_id}")
+            tasks = [send_meg(bot, gid, msg) for gid in forwarder_dest_group if gid != str(event.group_id)]
             await asyncio.wait(tasks)
